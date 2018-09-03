3996e9c801573ee5a40b80df9d27942092872910
moped <mos@mos.ase>
2018-09-03 12:17:34 +0100

    travis database fix

--- config/database.php
+++ config/database.php
@@ -4,21 +4,21 @@
  */
 
 // Local use
-// return [
-//     "dsn"             => "mysql:host=localhost;dbname=anaxdb;",
-//     "username"        => "anax",
-//     "password"        => "anax",
-//     "driver_options"  => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-//     "fetch_mode"      => \PDO::FETCH_OBJ,
-//     "table_prefix"    => null,
-//     "session_key"     => "Anax\Database",
-//
-//     // True to be very verbose during development
-//     "verbose"         => null,
-//
-//     // True to be verbose on connection failed
-//     "debug_connect"   => false,
-// ];
+return [
+    "dsn"             => "mysql:host=localhost;dbname=anaxdb;",
+    "username"        => "anax",
+    "password"        => "anax",
+    "driver_options"  => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
+    "fetch_mode"      => \PDO::FETCH_OBJ,
+    "table_prefix"    => null,
+    "session_key"     => "Anax\Database",
+
+    // True to be very verbose during development
+    "verbose"         => null,
+
+    // True to be verbose on connection failed
+    "debug_connect"   => false,
+];
 
 
 // student-servern
